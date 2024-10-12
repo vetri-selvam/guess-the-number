4762f30970fc10f6c72e93c6197c3aa8e3bc6c33
Vetri Selvam <vetriwork1@gmail.com>
2024-10-12 13:10:57 +0530

    setting logics and styling for App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Result from './Result';  
 
+const secretNum = Math.floor(Math.random *10) +1;
 function App() {
+  const [term, setTerm] = useState("");
+  
+  const handleChange= 
+      (e) => {
+        setTerm(e.target.value)
+      }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='head'>        
+          <label htmlFor='term'>Enter a number between 1 to 10:</label>
+        </div>
+        <input
+          id='term'
+          type='text'
+          onChange={handleChange}
+        />
+      </div>
+      <Result/>
     </div>
   );
 }
